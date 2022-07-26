78341c189d828ed0914c33e87c785ec5bed1315f
Jan F Kowalski <jan.kowalski@7thsense.de>
2022-07-26 15:03:22 +0200

    [WIP] Works. Need to style, calcProgress, check for the  labels. See what with GithubClient - not used at all.

--- public/index.php
+++ public/index.php
@@ -1,13 +1,12 @@
 <?php
 
 use App\KanbanBoard\Authentication;
-use App\KanbanBoard\GithubClient;
+//use App\KanbanBoard\GithubClient;
 use App\KanbanBoard\Utilities;
 use Github\AuthMethod;
 use Symfony\Component\Dotenv\Dotenv;
 use App\KanbanBoard\Application;
-use Laminas\Http\Client as ApiClient;
-use Symfony\Component\HttpClient\HttplugClient;
+use Github\Client as ApiClient;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -18,10 +17,10 @@ $repositories = explode('|', Utilities::env('GH_REPOSITORIES'));
 $authentication = new Authentication();
 $token = $authentication->login();
 
-$github = new \Github\Client();
+$github = new ApiClient();
 $github->authenticate($token,null,AuthMethod::ACCESS_TOKEN);
 
-$app = new Application($github, $repositories, array('waiting-for-feedback'));
+$app = new Application($github, $repositories, ['waiting-for-feedback','paused']);
 
 $board=$app->board();
 
@@ -29,39 +28,4 @@ $m = new Mustache_Engine(array(
     'loader' => new Mustache_Loader_FilesystemLoader('../views'),
 ));
 
-echo $m->render('index', $data);
-
-
-//$issues= $github->api('issue')->milestones()->all(Utilities::env('GH_ACCOUNT'));
-//var_dump($issues);die();
-//$board=$app->board();
-//die('elo');
-foreach($repositories as $repository) {
-    $milestones[]= $github->api('issue')->milestones()->all(Utilities::env('GH_ACCOUNT'),$repository);
-//    $issues= $github->api('issues')->all(Utilities::env('GH_ACCOUNT'),$repository,['state'=>'all']);
-}
-echo '<pre>';
-var_dump($milestones);die();
-$data=[];
-if( ! empty($issues)) {
-    foreach($issues as $issue) {
-        if(array_key_exists('milestone',$issue)) {
-            $data['issues'][$issue['state']]['data'][]=$issue;
-        }
-    }
-    if( array_key_exists('issues',$data) && ! empty($data['issues'])) {
-        foreach($data['issues'] as $issueState=>$issue) {
-            $data['issues'][$issueState]['length']=count($issue['data']);
-        }
-    }
-}
-
-
-echo "<pre>";var_dump($data);
-
-$m = new Mustache_Engine(array(
-    'loader' => new Mustache_Loader_FilesystemLoader('../views'),
-));
-
-echo $m->render('index', $data);
-
+echo $m->render('index', $board);
--- src/KanbanBoard/Application.php
+++ src/KanbanBoard/Application.php
@@ -2,55 +2,120 @@
 namespace App\KanbanBoard;
 
 use Michelf\Markdown;
+use Github\Client as ApiClient;
 
 class Application {
 
-    protected $github;
-    protected $repositories;
-    public $paused_labels;
+    protected ApiClient $github;
+    protected array $repositories;
+    public ?array $paused_labels;
 
-
-	public function __construct($github, $repositories, $paused_labels = array())
+	public function __construct( ApiClient $github, array $repositories, ?array $paused_labels = array())
 	{
 		$this->github = $github;
 		$this->repositories = $repositories;
 		$this->paused_labels = $paused_labels;
 	}
 
-	public function board()
+	public function board(): array
 	{
-		$ms = array();
-		foreach ($this->repositories as $repository)
-		{
-            $allMsApiData = $this->github->api('issue')->milestones()->all(Utilities::env('GH_ACCOUNT'),$repository);
-            foreach($allMsApiData as $data)
-			{
-				$ms[$data['title']] = $data;
-				$ms[$data['title']]['repository'] = $repository;
-			}
-		}
-        var_dump($ms);die();
-		ksort($ms);
-		foreach ($ms as $name => $data)
-		{
-			$issues = $this->issues($data['repository'], $data['number']);
-			$percent = self::_percent($data['closed_issues'], $data['open_issues']);
-			if($percent)
-			{
-				$milestones[] = array(
-					'milestone' => $name,
-					'url' => $data['html_url'],
-					'progress' => $percent,
-					'queued' => $issues['queued'],
-					'active' => $issues['active'],
-					'completed' => $issues['completed']
-				);
-			}
-		}
-		return $milestones;
+        // read Milestones for every Repository
+        $data['milestones'] = $this->getMilestones();
+
+        $issues = $this->getIssues();
+        $data['issues']=$this->fetchIssuesByStatus($issues);
+
+		return $data;
 	}
 
-	private function issues($repository, $milestone_id)
+    protected function getMilestones(): array
+    {
+        $ms=[];
+        if( ! empty($this->repositories)) {
+            foreach ($this->repositories as $repository)
+            {
+                $msByRepo = $this->github->api('issue')->milestones()->all(Utilities::env('GH_ACCOUNT'),$repository);
+                if( ! empty($msByRepo)) {
+                    foreach($msByRepo as $data) {
+                        $ms[$data['id']]=[
+                            'id'=>filter_var($data['id'],FILTER_SANITIZE_NUMBER_INT),
+                            'number'=>filter_var($data['number'],FILTER_SANITIZE_NUMBER_INT),
+                            'repository_name'=>$repository,
+                            'title'=>htmlspecialchars($data['title']),
+                            'description'=>Markdown::defaultTransform(
+                                htmlspecialchars($data['description'])
+                            ),
+                            'url'=>filter_var($data['url'],FILTER_SANITIZE_URL)
+                        ];
+                    }
+                }
+            }
+
+            ksort($ms);
+        }
+
+        return $ms;
+    }
+
+    protected function getIssues(): array
+    {
+        $data=[];
+        if( ! empty($this->repositories)) {
+            foreach ($this->repositories as $repository) {
+                $issuesByRepo = $this->github->api('issues')->all(Utilities::env('GH_ACCOUNT'),$repository,['state'=>'all']);
+                if( ! empty($issuesByRepo)) {
+                    $data = array_merge($data,$issuesByRepo);
+                }
+            }
+        }
+
+        return $data;
+    }
+
+    protected function fetchIssuesByStatus(array $issues): array
+    {
+        $issuesByStatus=[];
+        if( ! empty($issues)) {
+            foreach($issues as $issue) {
+                if ($issue['state']==='closed') {
+                    $state='completed';
+                } elseif( ! empty($issue['assignee'])) {
+                    $state='active';
+                } else {
+                    $state='queued';
+                }
+                $issuesByStatus[$state]['data'][]=[
+                    'id' => $issue['id'],
+                    'number' => $issue['number'],
+                    'title' => $issue['title'],
+				    'body' => Markdown::defaultTransform($issue['body']),
+                    'url' => $issue['html_url'],
+                    'progress' => $this->calcIssueProgress($issue['body']),
+                    'paused' => $this->isIssuePaused($issue['labels'])
+                ];
+            }
+
+            if( ! empty($issuesByStatus)) {
+                foreach($issuesByStatus as $state=>$issues) {
+                    $issuesByStatus[$state]['length']=count($issues['data']);
+                }
+            }
+        }
+
+        return $issuesByStatus;
+    }
+
+    protected function calcIssueProgress(): int
+    {
+        return 0;
+    }
+
+    protected function isIssuePaused($issueLabels)
+    {
+        return false;
+    }
+
+	protected function issues($repository, $milestone_id)
 	{
 		$i = $this->github->issues($repository, $milestone_id);
 		foreach ($i as $ii)
@@ -75,43 +140,43 @@ class Application {
 		});
 		return $issues;
 	}
+//
+//	private static function _state($issue)
+//	{
+//		if ($issue['state'] === 'closed')
+//			return 'completed';
+//		else if (Utilities::hasValue($issue, 'assignee') && count($issue['assignee']) > 0)
+//			return 'active';
+//		else
+//			return 'queued';
+//	}
 
-	private static function _state($issue)
-	{
-		if ($issue['state'] === 'closed')
-			return 'completed';
-		else if (Utilities::hasValue($issue, 'assignee') && count($issue['assignee']) > 0)
-			return 'active';
-		else
-			return 'queued';
-	}
+//	private static function labels_match($issue, $needles)
+//	{
+//		if(Utilities::hasValue($issue, 'labels')) {
+//			foreach ($issue['labels'] as $label) {
+//				if (in_array($label['name'], $needles)) {
+//					return array($label['name']);
+//				}
+//			}
+//		}
+//		return array();
+//
+//	}
 
-	private static function labels_match($issue, $needles)
-	{
-		if(Utilities::hasValue($issue, 'labels')) {
-			foreach ($issue['labels'] as $label) {
-				if (in_array($label['name'], $needles)) {
-					return array($label['name']);
-				}
-			}
-		}
-		return array();
-
-	}
-
-	private static function _percent($complete, $remaining)
-	{
-		$total = $complete + $remaining;
-		if($total > 0)
-		{
-			$percent = ($complete OR $remaining) ? round($complete / $total * 100) : 0;
-			return array(
-				'total' => $total,
-				'complete' => $complete,
-				'remaining' => $remaining,
-				'percent' => $percent
-			);
-		}
-		return array();
-	}
+//	private static function _percent($complete, $remaining)
+//	{
+//		$total = $complete + $remaining;
+//		if($total > 0)
+//		{
+//			$percent = ($complete OR $remaining) ? round($complete / $total * 100) : 0;
+//			return array(
+//				'total' => $total,
+//				'complete' => $complete,
+//				'remaining' => $remaining,
+//				'percent' => $percent
+//			);
+//		}
+//		return array();
+//	}
 }
