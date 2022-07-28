e47479341425115012913ad6759432584f54b17c
Jan F Kowalski <jan.kowalski@7thsense.de>
2022-07-28 18:04:25 +0200

    [#29] removed comments from di.php

--- config/di.php
+++ config/di.php
@@ -6,9 +6,9 @@ use DI\Container;
 use Firebase\JWT\JWT;
 
 return [
-    'gh.client.id' => DI\env('GH_CLIENT_ID'),//Utilities::env('GH_CLIENT_ID'),
-    'gh.client.secret' => DI\env('GH_CLIENT_SECRET'),//Utilities::env('GH_CLIENT_SECRET'),
-    'gh.client.alg' => DI\env('GH_ALG'),//Utilities::env('GH_ALG'),
+    'gh.client.id' => DI\env('GH_CLIENT_ID'),
+    'gh.client.secret' => DI\env('GH_CLIENT_SECRET'),
+    'gh.client.alg' => DI\env('GH_ALG'),
     'JWT' => new JWT(),
     'Authentication' => function (Container $c) {
         return new Authentication(
